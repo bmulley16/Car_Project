9786802d08475d98e70d45027ae464e6c8babda2
Bryce Mulley <bmulley9816@gmail.com>
2023-01-26 13:59:25 -0330

    making the pop up window to display parts

--- script/main.js
+++ script/main.js
@@ -1,7 +1,23 @@
 "use strict";
 
-let yearDropdownElement = document.querySelector(".year-of-make");
-yearDropdownElement.addEventListener("change", function (e) {
-  e.preventDefault();
-  console.log(yearDropdownElement.value);
+// let yearDropdownElement = document.querySelector("#year-of-make");
+// yearDropdownElement.addEventListener("change", function (e) {
+//   e.preventDefault();
+//   console.log(yearDropdownElement.value);
+// });
+
+// making a pop up that displays the car parts when submit is clicked
+
+const btn = document.querySelector(".btn");
+const partsOverlay = document.querySelector("#partsOverlay");
+const output = document.querySelector("#output");
+
+btn.addEventListener("click", function () {
+  partsOverlay.classList.remove("hidden");
+  output.classList.remove("hidden");
+});
+
+partsOverlay.addEventListener("click", function () {
+  partsOverlay.classList.add("hidden");
+  output.classList.add("hidden");
 });
