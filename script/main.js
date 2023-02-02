408cf85b3ee67cca8579b92498fd14910e39d7d4
Bryce Mulley <bmulley9816@gmail.com>
2023-02-02 16:07:33 -0330

    tried to make the model vehicle specific

--- script/main.js
+++ script/main.js
@@ -1,9 +1,7 @@
 "use strict";
 
-// let yearDropdownElement = document.querySelector("#year-of-make");
-// yearDropdownElement.addEventListener("change", function (e) {
-//   e.preventDefault();
-//   console.log(yearDropdownElement.value);
+// document.addEventListener("DOMContentLoaded", function () {
+//   console.log("dom has been loaded");
 // });
 
 // making a pop up that displays the car parts when submit is clicked
@@ -11,8 +9,47 @@
 const btn = document.querySelector(".btn");
 const partsOverlay = document.querySelector("#partsOverlay");
 const output = document.querySelector("#output");
-const exit = document.querySelector("#exitPartdisplay");
+const exit = document.querySelector("#exitPartDisplay");
+const yearOfMake = document.querySelector("#year-of-make");
 
+const makeDropdown = document.querySelector("#Make");
+const makes = ["Mazda", "Dodge", "Chevrolet"];
+const year = [1999, 2008, 1984];
+const category = ["Belt Drive", "Accessories"];
+const carModels = {
+  mazda: ["Speed 3"],
+  dodge: ["Ram 2500"],
+  chevrolet: ["K1500"],
+};
+// populating loops
+
+// loop for the make:
+
+for (let i = 0; i < makes.length; i++) {
+  let make = makes[i];
+  let el = document.createElement("option");
+  el.textContent = make;
+  el.value = make;
+  makeDropdown.appendChild(el);
+}
+// });
+
+// loop for the model:
+
+makeDropdown.addEventListener("change", function (e) {
+  const makeSelected = e.target.value.toLocaleLowerCase();
+  if (makeSelected === "mazda") {
+    for (let i = 0; i < carModels.mazda.length; i++) {
+      let model = carModels.mazda[i];
+      let el = document.createElement("option");
+      document.querySelector("#Model").appendChild(el);
+    }
+  }
+});
+// have event listener that listens to the changes current option of the make dropdown
+makeDropdown.addEventListener("change", function (e) {
+  console.log(e.target.value);
+});
 btn.addEventListener("click", function () {
   partsOverlay.classList.remove("hidden");
   output.classList.remove("hidden");
@@ -24,10 +61,6 @@ const exitBox = function () {
   partsOverlay.classList.add("hidden");
   output.classList.add("hidden");
 };
-
-partsOverlay.addEventListener("click", function () {
-  exitBox();
-});
 
 exit.addEventListener("click", function () {
   exitBox();
